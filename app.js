039ecfb94d5904b3fe572c10515987895064c2e0
Tushar Sehwag <tusharsehwag@Tushars-MacBook-Pro.local>
2021-08-02 01:31:28 +0530

    Validation Fixed

--- app.js
+++ app.js
@@ -12,3 +12,28 @@ function displaySpouse(status) {
     spouseBlock = document.getElementById("spouse");
     spouseBlock.style.display = "none";
   }
+
+  function validateForm() {
+    let firstName = document.forms["newForm"]["fname"].value;
+    let noOfWordsFname=firstName.split(" ").length;
+    if (noOfWordsFname>1) {
+      document.querySelector(".fname-space").style.display = "block";
+      return false;
+    }
+  
+    let spouseName= document.forms["newForm"]["spouse"].value;
+    let noOfWordsSpouse= spouseName.split(" ").length;
+    if (noOfWordsSpouse>1) {
+      document.querySelector(".spouse-space").style.display = "block";
+      return false;
+    }
+
+    document.getElementById("newForm").style.display="none";
+    document.querySelector(".success").style.display="block";
+  }
+
+  function closeHandler(){
+    document.getElementById("newForm").style.display="block";
+    document.querySelector(".success").style.display="none";
+
+  }
